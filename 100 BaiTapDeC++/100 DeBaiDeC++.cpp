108b38495f65f4b756ef9cdc988dd64bc91cc55c
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2024-10-10 07:21:51 +0700

    Add files via upload

---
+++ 100 BaiTapDeC++/100 DeBaiDeC++.cpp
@@ -0,0 +1,68 @@
+//1 Viết chương trình tính chu vi và diện tích hình vuông, với cạnh hình vuông a là số nguyên theo công thức cv=a*4, dt=a*a
+
+//2 Viết chương trình tính chu vi và diện tích hình tròn, với bán kính r là số nguyên theo công thức cv=r*2*3.14, dt=r*r*3.14.
+
+//3 Viết chương trình tính chu vi và diện tích hình chữ nhật, với chiều dài a và chiều rộng b là các số tự nhiên theo công thức cv=(a+b)*2, dt=a*b.
+
+//4 Viết chương trình tính tổng và tích 2 số nguyên a và b
+
+//5 Viết chương trình tính trung bình cộng của 3 số nguyên a, b, c theo công thức TBC=(a+b+c)/3
+
+/*6 Viết chương trình tính chu vi (cv) và diện tích (s) một tam giác có 3 cạnh a, b, c là các số nguyên 
+theo công thức cv=a+b+c, s=sqrt(p*(p-a)*(p-b)*(p-c)) với p=cv/2 (3 số a, b, c bảo đảm là 3 cạnh của 1 tam giác). */
+
+//7 Viết chương trình tính diện tích tam giác có chiều dài cạnh đáy a và chiều cao h tương ứng là các số nguyên theo công thức s=a*h/2.
+
+//8 Viết chương trình tính bán kính R của đường tròn ngoại tiếp tam giác có ba cạnh a,b,c  là các số nguyên theo công thức R=Image, với S là diện tích tam giác (3 số a, b, c bảo đảm là 3 cạnh của 1 tam giác).
+
+//9 Viết chương trình nhập vào điểm Toán, Văn, Anh văn của 1 học sinh là các số thực. Tính trung bình của học sinh đó, biết rằng điểm Toán, Văn là hệ số 2
+
+//10 Viết chương trình nhập vào số học sinh của 1 lớp (HS) và số táo trong rổ (T). 
+//Hãy chia đều số táo cho tất cả học sinh trong lớp, mỗi bạn sẽ được bao nhiêu quả táo? Còn dư lại bao nhiêu quả?
+
+//11 Viết chương trình nhập vào tổng số giây là một số nguyên, in ra giờ:phút:giây.
+
+//12 Cho một số nguyên có 3 chữ số. In ra 2 chữ số cuối và 2 chữ số đầu của số vừa nhập
+
+//13 Viết chương trình nhập vào 3 số nguyên a, b, c; in ra số lớn nhất, nếu 3 số bằng nhau thì in dấu "="
+
+//14 
+//Cho tuổi của một người (0<tuổi<=150), trả lời:
+
+//“Thieu nhi” nếu 0< tuổi<=11
+
+//“Thieu nien” nếu 11< tuổi <=25
+
+//“Trung nien” nếu 25< tuổi <=50
+
+//“Lao nien” nếu tuổi >50
+
+//15 Viết chương trình kiểm tra xem 3 số a, b, c nhập từ bàn phím có là 3 cạnh của một tam giác hay ko? 
+//Nếu phải thì tính chu vi và diện tích của tam giác đó, ngược lại thông báo 3 số đó ko phải là 3 cạnh của tam giác
+
+//16 Viết chương trình nhập vào điểm trung bình học kỳ I và trung bình học kỳ II, tính trung bình cả năm theo công thức:  TBCN=(TBHKI+TBHKII*2)/3. Xếp lọai theo cách:
+
+/* “Gioi” nếu TBCN>=8
+
+“Kha” nếu 6.5<=TBCN <8
+
+“Trung binh” nếu 5.0<=TBCN <6.5
+
+“Yeu” nếu 3.5<=TBCN <5
+
+“Kem” nếu TBCN<3.5 */ 
+
+//17 Nhập một tháng (tây), cho biết tháng đó thuộc mùa nào 
+//(Xuân, Hạ, Thu, Ðông), biết rằng tháng 2, 3, 4 là mùa Xuân, tháng 5, 6, 7: mùa Hạ, tháng 8, 9, 10: mùa Thu, và tháng 11, 12, 1: mùa Ðông.
+
+//18 Viết chương trình nhập vào 1 tháng và năm, cho biết tháng đó có bao nhiêu ngày 
+//(Năm nhuận là năm chia hết cho 400 hoặc chia hết cho 4 nhưng không chia hết cho 100).
+
+//19 Nhập vào số xe (có năm chữ số) trong biển số xe máy của bạn. Cho biết số xe của bạn được mấy nút. Bạn là người may mắn nếu số nút=9. 
+//Bạn có may mắn không?
+//Ví dụ: số xe là 45046, tổng các chữ số là 4+ 5 +0+ 4 + 6 = 19 thì 19 mod 10 =9. Số nút là 9: may man. Số xe 12340, 
+//tổng các chữ số là 1+2+3+4+0=10 thì 10 mod 10=0. Số nút là 0: chua may man
+
+//20 Nhập một số tiền N (N<=1.000.000) đồng (N là số chia hêt cho 1.000), đổi ra xem được bao nhiêu tờ 5.000 đồng, 
+//bao nhiêu tờ 2.000 đồng, bao nhiêu tờ 1.000 đồng sao cho tổng số tờ là ít nhất. 
+//Ví dụ N= 43.000 đ = 8 tờ 5.000 đ + 1 tờ 2.000 đ + 1 tờ 1.000 đ.
